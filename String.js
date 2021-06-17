7590d64914a4be3e8edbbeaf61e70a31844b5c93
mztkn <lijiachen633@gmail.com>
2021-06-18 00:15:21 +0800

    求最长回文串

--- String.js
+++ String.js
@@ -14,5 +14,29 @@ const lengthOfLongestSubstring = s => {
   }
   return max
 }
-console.log(lengthOfLongestSubstring(' '))
+// console.log(lengthOfLongestSubstring(' '))
 
+/**
+ * @description 求最长回文串
+ * @param {string} s
+ * @return {string}
+ */
+const getPalindrome = s => {
+  let n = s.length
+  let res = ''
+  let dp = Array.from(new Array(n), () => new Array(n).fill(0)) // 生成一个二维数组记录回文串的状态
+  // 回文串满足的条件 对应i和j下标的值相，相应的i+1和j-1也相等
+  // 如果s长度=0或1那本身就是回文串
+  for (let i = n - 1; i >= 0; i--) {
+    for (let j = i; j < n; j++) {
+      dp[i][j] = s[i] == s[j] && (j - i < 2 || dp[i + 1][j - 1])
+      if (dp[i][j] && j - i + 1 > res.length) {
+        res = s.substring(i, j + 1)
+      }
+    }
+  }
+  return res
+
+}
+
+console.log(getPalindrome('acdfd'))
