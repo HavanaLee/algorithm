79f004ca13484f4a5616387ae0cf8fb052bbd95d
mztkn <lijiachen633@gmail.com>
2021-06-24 23:27:05 +0800

    盛最多的水

--- Array.js
+++ Array.js
@@ -15,3 +15,20 @@ const twoSums = (nums, target) => {
   }
 }
 
+/**
+ * @description 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0) 。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let l = 0, r = height.length - 1
+  let ans = 0
+  while (l < r) {
+    const area = Math.min(height[l], height[r]) * (r - l)
+    ans = Math.max(ans, area)
+    height[l] >= height[r] ? --r : ++l
+  }
+  return ans
+
+}
+
