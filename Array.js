0e2c3c56f2ddc511a846a18bb39b5af18f2441f7
mztkn <lijiachen633@gmail.com>
2021-06-30 22:41:53 +0800

    数组：移除元素

--- Array.js
+++ Array.js
@@ -49,3 +49,19 @@ const search = (nums, target) => {
   return -1 // 没有找到结果，返回-1
 }
 
+/**
+ * @description 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+ * @type 快慢指针
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+const removeElement = function (nums, val) {
+  let slow = 0
+  for (let i = 0; i < nums.length; i++) {
+    if (val != nums[i]) {
+      nums[slow++] = nums[i]
+    }
+  }
+  return slow
+}
