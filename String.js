66952dfc3a78b89af944ac48417b2cbd3d1d6393
mztkn <lijiachen633@gmail.com>
2021-08-07 00:37:08 +0800

    add:反转字符串 II

--- String.js
+++ String.js
@@ -72,4 +72,23 @@ var reverseString = function (s) {
   return s
 }
 
+/**
+ * @description 给定一个字符串 s 和一个整数 k，从字符串开头算起，每 2k 个字符反转前 k 个字符。如果剩余字符少于 k 个，则将剩余字符全部反转。如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var reverseStr = function (s, k) {
+  let arr = s.split('')
+  for (let i = 0; i < s.length; i += (k * 2)) {
+    let l = i - 1, r = i + k > s.length ? s.length : i + k
+    while (--r > ++l) {
+      [arr[l], arr[r]] = [arr[r], arr[l]]
+    }
+  }
+  return arr.join('')
+}
+
+reverseStr('abcdefg', 2)
+
 console.log(getPalindrome('acdfd'))
