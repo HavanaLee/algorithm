1399bed3f86695d266882d6287615b5329d5e637
mztkn <lijiachen633@gmail.com>
2021-12-10 20:10:18 +0800

    27. 移除元素

--- Array.ts
+++ Array.ts
@@ -35,23 +35,6 @@
 
 
 // /**
-//  * @description 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
-//  * @type 快慢指针
-//  * @param {number[]} nums
-//  * @param {number} val
-//  * @return {number}
-//  */
-// const removeElement = function (nums, val) {
-//   let slow = 0
-//   for (let i = 0; i < nums.length; i++) {
-//     if (val != nums[i]) {
-//       nums[slow++] = nums[i]
-//     }
-//   }
-//   return slow
-// }
-
-// /**
 //  * @description 给你一个按 非递减顺序 排序的整数数组 nums，返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序。
 //  * @type 双指针
 //  * @param {number[]} nums
@@ -364,6 +347,18 @@ function search(nums: number[], target: number): number {
     else if (nums[m] > target) r = m - 1
   }
   return -1
+};
+
+/**
+ * @link https://leetcode-cn.com/problems/remove-element/
+ * @method 双指针 时间复杂度O(n)，空间复杂度O(1)
+ */
+function removeElement(nums: number[], val: number): number {
+  let index = 0
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) nums[index++] = nums[i]
+  }
+  return index
 };
 smallerNumbersThanCurrent([8, 1, 2, 2, 3])
 search([-1, 0, 3, 5, 9, 12], 9)
