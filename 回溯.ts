f37e9025f8b9299a1fb9bfd271578c0cca91b697
mztkn <lijiachen633@gmail.com>
2022-11-14 19:17:11 +0800

    面试题 08.09. 括号

--- 回溯.ts
+++ 回溯.ts
@@ -44,3 +44,40 @@ function permutation(S: string): string[] {
   }
   return res
 };
+
+/**
+ * @link https://leetcode.cn/problems/bracket-lcci/?favorite=xb9lfcwi
+ */
+function generateParenthesis(n: number): string[] {
+  let res: string[] = [], temp = Array.from({ length: n * 2 }, (v, k) => k % 2 ? ')' : '('), len = temp.length
+  dfs(1)
+  function dfs(k: number) {
+    if (!isValid(temp.slice(0, k + 1))) return
+    if (k == len - 1) {
+      res.push(temp.join(''))
+      return
+    }
+    let set = new Set<string>()
+    for (let i = k; i < len - 1; i++) {
+      if (!set.has(temp[i])) {
+        [temp[i] as '(' | ')', temp[k] as '(' | ')'] = [temp[k], temp[i]]
+        dfs(k + 1);
+        [temp[i] as '(' | ')', temp[k] as '(' | ')'] = [temp[k], temp[i]];
+        set.add(temp[i])
+      }
+    }
+  }
+  function isValid(temp: ('(' | ')')[]) {
+    let stack: string[] = []
+    for (let i = 0; i < temp.length; i++) {
+      if (temp[i] === '(') stack.push(temp[i])
+      else {
+        if (stack.length) stack.pop()
+        else return false
+      }
+    }
+    return true
+  }
+  return res
+};
+generateParenthesis(3)
