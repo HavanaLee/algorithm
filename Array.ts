c1848da9f98e11d90d11df88089abfe43685b927
mztkn <lijiachen633@gmail.com>
2022-12-23 14:14:07 +0800

    2011. 执行操作后的变量值

--- Array.ts
+++ Array.ts
@@ -850,6 +850,22 @@ function findClosest(words: string[], word1: string, word2: string): number {
   return min
 };
 
+/**
+ * @link https://leetcode.cn/problems/final-value-of-variable-after-performing-operations/
+ */
+
+function finalValueAfterOperations(operations: string[]): number {
+  let obj: { [key in string]: number } = {
+    '++X': 1,
+    'X++': 1,
+    '--X': -1,
+    'X--': -1
+  }, ans = 0
+  for (const opt of operations) {
+    ans += obj[opt]
+  }
+  return ans
+};
 maxAliveYear([1972, 1908, 1915, 1957, 1960, 1948, 1912, 1903, 1949, 1977, 1900, 1957, 1934, 1929, 1913, 1902, 1903, 1901], [1997, 1932, 1963, 1997, 1983, 2000, 1926, 1962, 1955, 1997, 1998, 1989, 1992, 1975, 1940, 1903, 1983, 1969])
 
 
