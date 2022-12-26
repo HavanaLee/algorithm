70044e9cd4790350d902c9e3f37641fe1e7ed30a
mztkn <lijiachen633@gmail.com>
2022-12-26 21:09:36 +0800

    1759. 统计同构子字符串的数目

--- Array.ts
+++ Array.ts
@@ -866,6 +866,26 @@ function finalValueAfterOperations(operations: string[]): number {
   }
   return ans
 };
+
+/**
+ * @link https://leetcode.cn/problems/count-number-of-homogenous-substrings/
+ */
+function countHomogenous(s: string): number {
+  let i = 1, sum = 0, cur = 1
+  const MOD = 1000000007
+  while (i < s.length) {
+    if (s[i] === s[i - 1]) {
+      cur++
+    } else {
+      // 实际上是一个等差数列, (m + 1) * m / 2
+      sum += (cur + 1) * cur / 2
+      cur = 1
+    }
+    i++
+  }
+  sum += (cur + 1) * cur / 2
+  return sum % MOD
+};
 maxAliveYear([1972, 1908, 1915, 1957, 1960, 1948, 1912, 1903, 1949, 1977, 1900, 1957, 1934, 1929, 1913, 1902, 1903, 1901], [1997, 1932, 1963, 1997, 1983, 2000, 1926, 1962, 1955, 1997, 1998, 1989, 1992, 1975, 1940, 1903, 1983, 1969])
 
 
